418d8d908f4d13475d5f0adfe94bca628b82a14c
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-10-20 14:10:52 +0700

    first commit

--- demo-project-v1/backend/index.js
+++ demo-project-v1/backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const cors = require("cors");
+const clc = require("cli-color");
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(
@@ -12,10 +16,15 @@ app.use(
 
 app.use(morgan("dev"));
 app.use(morgan("combined"));
-app.use(helmet())
+app.use(helmet());
 
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "Hello world",
+  });
+});
 
-
-app.listen(8080, () => {
-
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server is running on: ", clc.blue(`http://localhost:${PORT}`));
 });
