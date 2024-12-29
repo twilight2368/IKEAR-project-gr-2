9dfc1225bf890f04d9409f6a7c0165571d70906f
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-12-29 09:43:28 +0700

    feat: :construction: update controllers, routes, connect to database, configs

--- services/service2/src/models/connect.js
+++ services/service2/src/models/connect.js
@@ -1,1 +1,24 @@
 const mongoose = require("mongoose");
+const clc = require("cli-color");
+const Item = require("./schemas/Item");
+const Store = require("./schemas/Store");
+const Inventory = require("./schemas/Inventory");
+const DB_CONFIG = require("../configs/db.config");
+
+const CONNECT_STRING = DB_CONFIG.stringConnect;
+
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(CONNECT_STRING);
+    console.log("====================================");
+    console.log(clc.green("Connected to MongoDB successfully"));
+    console.log("====================================");
+  } catch (error) {
+    console.log("====================================");
+    console.log(clc.red("Error connecting to MongoDB"));
+    console.error(error);
+    console.log("====================================");
+  }
+};
+
+module.exports = connectMongoDB;
